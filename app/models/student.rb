987512a04b56c6e3602a13efa594eaadd4bccaeb
Dave Kubasiak <dkubasiak@comverge.com>
2017-03-28 12:05:49 -0400

    * Added validations to Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ApplicationRecord
+  validates :first_name, :last_name, :email, :cell_phone, :home_phone, :work_phone, presence: true
+  validates :email, format: {with: /.+@.+\..+/i}
+  validates :cell_phone, :home_phone, :work_phone, format: {with: /\d{3}-\d{3}-\d{4}/}
 end
